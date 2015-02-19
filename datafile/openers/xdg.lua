dbd8ec6e73513b5c73335b5ebe19bf733ff2af2c
Hisham Muhammad <hisham@gobolinux.org>
2015-02-19 21:48:41 -0200

    Add XDG_RUNTIME_DIR to xdg lookup paths.

--- datafile/openers/xdg.lua
+++ datafile/openers/xdg.lua
@@ -15,15 +15,19 @@ local function split(var)
 end
 
 local HOME = os.getenv("HOME")
-local XDG_DATA_HOME = os.getenv("XDG_DATA_HOME") or HOME.."/.local/share"
+local XDG_DATA_HOME   = os.getenv("XDG_DATA_HOME")   or HOME.."/.local/share"
+local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or HOME.."/.config"
+local XDG_CACHE_HOME  = os.getenv("XDG_CACHE_HOME")  or HOME.."/.cache"
+local XDG_RUNTIME_DIR = os.getenv("XDG_RUNTIME_DIR")
+
 local XDG_DATA_DIRS = split(os.getenv("XDG_DATA_DIRS") or "/usr/local/share:/usr/share")
 table.insert(XDG_DATA_DIRS, 1, XDG_DATA_HOME)
-local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or HOME.."/.config"
+table.insert(XDG_DATA_DIRS, XDG_RUNTIME_DIR)
+
 local XDG_CONFIG_DIRS = split(os.getenv("XDG_CONFIG_DIRS") or "/etc/xdg")
 table.insert(XDG_CONFIG_DIRS, 1, XDG_CONFIG_HOME)
-local XDG_CACHE_HOME = os.getenv("XDG_CACHE_HOME" or HOME.."/.cache")
-local XDG_CACHE_DIRS = { XDG_CACHE_HOME }
---local XDG_RUNTIME_DIR = os.getenv("XDG_RUNTIME_DIR")
+
+local XDG_CACHE_DIRS = { XDG_CACHE_HOME, XDG_RUNTIME_DIR }
 
 function xdg.opener(file, mode, context)
    if not file then
